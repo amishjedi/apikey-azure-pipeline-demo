406ca9522e462045dbeb358338f9ad0b9e2cb72d
amishjedi <54509612+amishjedi@users.noreply.github.com>
2020-07-26 14:54:16 -0400

    Create get_conjur_secret.sh

---
+++ get_conjur_secret.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Authenticates as host identity with API key and gets value of a specified variable
+#
+# URL, ACCOUNT, CERT_FILE, LOGIN and API_KEY are taken from build vars in library
+# API_KEY is passed as command line parameter because it is tagged as secret.
+
+################  MAIN   ################
+# Takes 2 arguments:
+#   $1 - api key
+#   $2 - name of variable to value to return
+#
+main() {
+
+  if [[ $# -ne 2 ]] ; then
+    printf "\nUsage: %s <api-key> <variable-name>\n" $0
+    exit -1
+  fi
+  local CONJUR_AUTHN_API_KEY=$1
+  local variable_name=$2
+				# authenticate, get ACCESS_TOKEN
+  ACCESS_TOKEN=$(authn_host $CONJUR_AUTHN_LOGIN $CONJUR_AUTHN_API_KEY)
+  if [[ "$ACCESS_TOKEN" == "" ]]; then
+    echo "Authentication failed..."
+    exit -1
+  fi
+
+  local encoded_var_name=$(urlify "$variable_name")
+  curl -s \
+	--cacert $CONJUR_CERT_FILE \
+	-H "Content-Type: application/json" \
+	-H "Authorization: Token token=\"$ACCESS_TOKEN\"" \
+     $CONJUR_APPLIANCE_URL/secrets/$CONJUR_ACCOUNT/variable/$encoded_var_name
+}
+
+##################
+# AUTHN HOST
+#  $1 - host identity
+#  $2 - API key
+#
+authn_host() {
+  local host_id=$1; shift
+  local api_key=$1; shift
+
+  local encoded_host_id=$(urlify "$host_id")
+  local response=$(curl -s \
+		     --cacert $CONJUR_CERT_FILE \
+                     --data $api_key \
+                     $CONJUR_APPLIANCE_URL/authn/$CONJUR_ACCOUNT/$encoded_host_id/authenticate)
+  access_token=$(echo -n $response| base64 | tr -d '\r\n')
+  echo "$access_token"
+}
+
+################
+# URLIFY - url encodes input string
+# in: $1 - string to encode
+# out: URLIFIED - global variable containing encoded string
+urlify() {
+        local str=$1; shift
+        str=$(echo $str | sed 's= =%20=g')
+        str=$(echo $str | sed 's=/=%2F=g')
+        str=$(echo $str | sed 's=:=%3A=g')
+        str=$(echo $str | sed 's=+=%2B=g')
+        str=$(echo $str | sed 's=&=%26=g')
+        str=$(echo $str | sed 's=@=%40=g')
+        echo $str
+}
+
+main "$@"
